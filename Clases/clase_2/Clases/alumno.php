3bfeaa41f82cab4fe4e4bb7c380fb808a7dd0f35
Lionel <lionherrero@gmail.com>
2019-08-27 21:17:30 -0300

    Clase 2

--- Clases/clase_2/Clases/alumno.php
+++ Clases/clase_2/Clases/alumno.php
@@ -6,7 +6,12 @@
 
         public function toString()
         {
-            return json_encode($this);
+            return json_encode($this); //devuelvo un string json
+        }
+
+        public static function mostrarListaAlumnos($alumnos)
+        {
+            return json_encode($alumnos);
         }
 
     }
--- Clases/clase_2/POO.php
+++ Clases/clase_2/POO.php
@@ -2,11 +2,11 @@
     require_once "./Clases/persona.php";
     require_once "./Clases/alumno.php";
     
-    $datos;
-    $nombre;
-    $apellido;
-    $alumno;
+    session_start();
+    //session_unset();
+    //$i = 0;
 
+    /*
     if(isset($_GET["Nombre"]) && isset($_GET["Apellido"]))
     {
         $nombre = $_GET["Nombre"];
@@ -17,6 +17,75 @@
         $datos = $alumno->toString();
     
         echo $datos;
+    }*/
+    
+    /*
+            $cantidad = $_POST["cantidad"];
+    
+        while($i < $cantidad)
+        {
+            $alumno = new Alumno("alumno " . ($i + 1),
+                        "apellido " . ($i + 1));
+            
+            $alumnos[$i] = $alumno;
+
+            $i++;
+        }
+    */
+    
+
+    /*
+    $datos = Alumno::mostrarListaAlumnos($alumnos);
+
+    echo $datos;*/
+
+    if($_SERVER['REQUEST_METHOD'] == 'GET')
+    {
+
+        $nombre = $_GET["Nombre"];
+        $apellido = $_GET["Apellido"];
+
+        $alumno = new Alumno($nombre, $apellido);
+
+        //$datos = $alumno->toString();
+
+        if(!isset($_SESSION["alumnos"]))
+        {
+            $alumnos = array($alumno);
+            $indice = 0;
+            $alumnos[$indice] = $alumno;
+        }
+        else
+        {
+            $alumnos = $_SESSION["alumnos"];
+
+            $indice = count($alumnos); 
+
+
+            $alumnos[$indice] = $alumno;
+        }
+
+        $_SESSION["alumnos"] = $alumnos;
+
+        echo "Alumno Cargado<br>";
+
+        //$datos = $alumno->toString();
+
+        //echo $datos;
+        
+    }
+    else if($_SERVER['REQUEST_METHOD'] == 'POST')
+    {
+        if(isset($_SESSION["alumnos"]))
+        {
+            $alumnos = $_SESSION["alumnos"];
+
+            $datos = Alumno::mostrarListaAlumnos($alumnos);
+
+            echo $datos;
+            echo count($alumnos);
+        }
+            
     }
    
 
