4980c9e72c6cbc51db4b119b66b879b4792fe7e8
Lionel <lionherrero@gmail.com>
2019-09-10 20:41:32 -0300

    Clase 4

---
+++ Clases/clase_04/abm/clases/archivo.php
@@ -0,0 +1,147 @@
+<?php
+    class Archivo
+    {
+        public static function GuardarArchivoTemporal($archivo, $destino)
+        {
+            
+            $origen = $archivo["tmp_name"];//ubicación temporal
+            $fecha = new DateTime();//timestamp para no repetir nombre
+            
+
+
+            //otro metodo: $_FILES["archivo"]["tmp_name"];
+
+            //busco la extensión del archivo:
+            $extension = pathinfo($_FILES["archivo"]["name"], PATHINFO_EXTENSION);
+
+            echo $extension;
+
+            $destino = $destino . "\\archivo" . $fecha->getTimeStamp() . "." . $extension;
+
+            //lo saco de la carpeta temporal:
+            move_uploaded_file($origen, $destino);
+        }
+
+
+
+        public static function Borrar($ruta, $nroLegajo)
+        {
+            $datos = Archivo::Leer($ruta);
+
+            for($i = 0; $i < count($datos); $i++)
+            {
+                $objeto = $datos[$i];
+
+                if($objeto->legajo == $nroLegajo)
+                {
+                    unset($datos[$i]);
+                    array_values($datos); //indices correlativos
+                    break;
+                }
+
+            }
+
+            //guardo los datos de nuevo en el archivo
+            foreach($datos as $objeto)
+            {
+                // $objeto = $datos[$i];
+
+                unlink("objetos.json");//borro el archivo anterior
+
+                Archivo::Guardar($ruta, $objeto);
+            }
+        }
+
+        public static function Modificar($ruta, $elementoModificado)
+        {
+            $datos = Archivo::Leer($ruta);
+
+            for($i = 0; $i < count(); $i++)
+            {
+                $objeto = $datos[$i];
+
+                if($objeto->legajo == $elementoModificado["legajo"])
+                {
+                    $datos[$i] = $elementoModificado;
+                }
+            }
+
+            //guardo los datos de nuevo en el archivo
+            for($i= 0 ; $i < count($datos); $i++)
+            {
+                $objeto = $datos[$i];
+
+                unlink("objetos.json");
+
+                Archivo::Guardar($objeto);
+            }
+        }
+
+        public static function Guardar($ruta, $dato)
+        {
+            $ar = fopen($ruta, "a");
+
+            fwrite($ar, json_encode($dato) . PHP_EOL);
+
+            fclose($ar);
+
+        }
+
+        public static function Leer($ruta)
+        {
+
+            $ar = fopen($ruta, "r");
+            $datos = array();//array donde voy a guardar los objetos
+
+
+
+            while(!feof($ar))
+            {
+                $objeto = json_decode(fgets($ar));//Se guarda como un objeto
+
+                if($objeto != null)
+                {
+                    array_push($datos, $objeto);
+                }
+
+
+            }
+
+            fclose($ar);
+
+            return $datos;
+        }
+
+        public static function Mostrar($ruta)
+        {
+          //mostrar
+
+          $datos = Archivo::Leer($ruta);
+
+
+          for($i = 0; $i < count($datos); $i++)
+          {
+              echo "Persona " . ($i+1) . "<br>";
+
+              $objeto = $datos[$i];
+
+              echo "Nombre: " . ucwords($objeto->nombre) . "<br>";
+              echo "Legajo: "  . $objeto->legajo  . "<br><br>";
+
+              //También:
+              // foreach($datos[$i] as $clave=>$valor)
+              // {
+              //      echo $clave . ": " . $valor . "<br>";
+              // }
+
+          }
+
+        }
+
+    }
+
+
+
+
+
+?>
---
+++ Clases/clase_04/abm/index.php
@@ -0,0 +1,70 @@
+<?php
+    require_once "./clases/archivo.php";
+
+    $opcion = $_POST["opcion"];
+    $ruta = "objetos.json";
+
+    switch($opcion)
+    {
+        case "guardar"://Guardar en archivo
+        if(isset($_POST["nombre"], $_POST["legajo"]))
+        {
+            $objeto = array("nombre" => $_POST["nombre"], "legajo" => $_POST["legajo"]);
+
+            Archivo::Guardar($ruta, $objeto);
+
+            Archivo::Mostrar($ruta);
+        }
+        break;
+
+        case "borrar"://borrar
+        if(isset($_POST["legajo"]))
+        {
+          $legajo = $_POST["legajo"];
+
+          Archivo::Borrar($ruta, $legajo);
+
+          Archivo::Mostrar($ruta);
+
+        }
+        break;
+
+        case "modificar"://Modificar
+        if(isset($_POST["nombre"], $_POST["legajo"]))
+        {
+          $objetoModificado = array("nombre" => $_POST["nombre"], "legajo" => $_POST["legajo"]);
+
+          Archivo::Modificar($ruta, $objetoModificado);
+
+          Archivo::Mostrar($ruta);
+
+        }
+        break;
+
+        case "mostrar"://Mostrar
+        Archivo::Mostrar($ruta);
+        break;
+
+        case "guardarArchivo":
+        Archivo::GuardarArchivoTemporal();
+        break;
+
+    }
+
+
+
+
+
+
+    // $datos = Leer("objetos.json");
+
+    // Mostrar($datos);
+
+    // // fwrite($ar, "Hola" . PHP_EOL); //constante PHP_EOL
+
+    // // copy("archivo.txt", "archivoCopia.txt"); //Copia archivo
+
+    // // unlink("archivoCopia.txt"); //borra archivo
+
+
+?>
